fcecb27e5dc7ef814fb1c4a27cb6ee2f68464501
ilyam <ilya.metelkin.2005@mail.ru>
2022-04-26 23:05:47 +0300

    /

---
+++ main.py
@@ -0,0 +1,10 @@
+sp = [int(s) for s in open('name file').readlines()]
+
+a = []
+
+for i in range(len(sp)-1):
+    #for j in range(len(sp)):        #для пар с различными элементами
+        if:
+            a.append(sp[i] + sp[i+1])
+
+print(len(a), max(a))
